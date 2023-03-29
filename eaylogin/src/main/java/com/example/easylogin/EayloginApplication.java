add4ce1a8732434a0507de1fe8a8dfdb215d7786
“yasue0706” <yutakayasue0706@gmail.com>
2023-03-29 14:19:43 +0900

    Lesson_ecsite

---
+++ eaylogin/src/main/java/com/example/easylogin/EayloginApplication.java
@@ -0,0 +1,13 @@
+package com.example.easylogin;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EayloginApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EayloginApplication.class, args);
+	}
+
+}
